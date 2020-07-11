0693c4fa15e4b6c66feac0898fa3a8279be0252c
Gordon McCreight <gordon@mccreight.com>
2020-07-11 15:35:39 -0700

    Switch to something with a grouping

--- examples/example_020_merge_streams/code/code_for_materialized.sh
+++ examples/example_020_merge_streams/code/code_for_materialized.sh
@@ -5,19 +5,25 @@ scratch_dir=$1
 stream_1_file=$scratch_dir/stream_1_file
 stream_2_file=$scratch_dir/stream_2_file
 
+touch $stream_1_file
+touch $stream_2_file
+
+sleep 5
+
 cat << EOF > $scratch_dir/stream_1_generator.sh
-for i in \`seq 1 10\`; do
-  echo data: {\"id\":\$i} >> $stream_1_file
-  sleep 1
+for i in \`seq 1 500\`; do
+  grouping_code=\$((\$i / 10))
+  echo data: {\"id\":\$i, \"grouping_code\": \"0000\$grouping_code\", \"code\": \"\$i\"} >> $stream_1_file
+  sleep 0.01
 done
 EOF
 
 chmod 755 $scratch_dir/stream_1_generator.sh
 
 cat << EOF > $scratch_dir/stream_2_generator.sh
-for i in \`seq 1 5\`; do
+for i in \`seq 1 50\`; do
   echo data: {\"id\":\$i} >> $stream_2_file
-  sleep 2
+  sleep 0.2
 done
 EOF
 
--- examples/example_020_merge_streams/code/code_for_tester.sh
+++ examples/example_020_merge_streams/code/code_for_tester.sh
@@ -10,6 +10,7 @@ stream_2_file=$scratch_dir/stream_2_file
 sleep 1
 
 psql -h matiz-materialized -p 6875 materialize > $log_file << EOF
+drop view if exists example_020_group_counts;
 drop view if exists example_020_stream_1_recentchanges;
 drop source if exists example_020_stream_1;
 EOF
@@ -23,7 +24,9 @@ EOF
 psql -h matiz-materialized -p 6875 materialize >> $log_file << EOF
 create materialized view example_020_stream_1_recentchanges as
     select
-        (val->'id')::float::int as id
+        (val->'id')::float::int as id,
+        val->>'grouping_code' AS grouping_code,
+        val->>'code' AS code
     from (select data::jsonb as val from example_020_stream_1)
 ;
 EOF
@@ -33,10 +36,34 @@ show columns from example_020_stream_1_recentchanges
 ;
 EOF
 
+psql -h matiz-materialized -p 6875 materialize >> $log_file << EOF
+create materialized view example_020_group_counts as
+select
+  grouping_code,
+  count(*) as num_codes,
+  max(id) as max_id
+from example_020_stream_1_recentchanges
+group by grouping_code
+;
+EOF
+
+
 for i in `seq 1 10`; do
+
+#   echo "looking for the recent changes $i"
+# psql -h matiz-materialized -p 6875 materialize << EOF
+# select *
+# from example_020_group_counts
+# order by max_id desc
+# limit 10
+# EOF
+
   echo "looking for the recent changes $i" >> $log_file
 psql -h matiz-materialized -p 6875 materialize >> $log_file << EOF
-select count(*) from example_020_stream_1_recentchanges
+select *
+from example_020_group_counts
+order by max_id desc
+limit 10
 EOF
 sleep 1
 done
@@ -44,5 +71,7 @@ done
 sleep 1
 
 psql -h matiz-materialized -p 6875 materialize > $result_file << EOF
-select count(*) from example_020_stream_1_recentchanges
+select num_codes
+from example_020_group_counts
+where grouping_code = '000049'
 EOF
